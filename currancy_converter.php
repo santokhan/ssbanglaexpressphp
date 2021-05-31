ede7cc5e6334069adc3d0533701720fabafe4b16
Santo Khan <santok54@gmail.com>
2021-05-31 18:36:56 +0600

    Delete currancy_converter.php

--- currancy_converter.php
+++
@@ -1,9 +0,0 @@
-<?php include "./component/header.php"; ?>
-
-<?php include "./component/slider.php"; ?>
-
-<?php include "component/card.php"; ?>
-
-<?php include "component/parallex.php"; ?>
-
-<?php include "component/footer.php"; ?>
