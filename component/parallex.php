eeb24b314af519291b448c1db16692f40efae23a
Santo Khan <santok54@gmail.com>
2021-05-31 18:32:56 +0600

    Create parallex.php

---
+++ component/parallex.php
@@ -0,0 +1,10 @@
+<!-- Parallex Scroll Effect Background-->
+<div class="container-fluid text-center bg-secondary py-5 parallax">
+    <span class="glass-effect"></span>
+    <div class="row py-5 my-5 text-white bg-transparent">
+        <div class="col py-5  bg-transparent">
+            <h3>WE CARE ABOUT YOUR CARGO / COURIER.</h3>
+            <p><strong>"Make your transportation fast and safe"</strong></p>
+        </div>
+    </div>
+</div>
