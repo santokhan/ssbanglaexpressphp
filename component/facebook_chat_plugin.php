5e2882d1cbc112eb92b4c4a725d6aba054401c26
Santo Khan <santok54@gmail.com>
2021-06-01 12:05:16 +0600

    Update facebook_chat_plugin.php

--- component/facebook_chat_plugin.php
+++ component/facebook_chat_plugin.php
@@ -24,3 +24,9 @@ window.fbAsyncInit = function() {
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 </script>
+
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0&appId=777518929519792&autoLogAppEvents=1" nonce="G7ReeypO"></script>
+
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0&appId=777518929519792&autoLogAppEvents=1" nonce="8koTaRq6"></script>
